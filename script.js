4e98a31d082ab6e9081d9d673f013c8b96123734
Gaetanerk <gaetan.turben@outlook.com>
2022-04-26 11:08:17 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let response = prompt("donne moi un chiffre compris entre 1 et 3");
+
+while (response < 1 || response > 3) {
+  response = prompt("La valeur doit être comprise entre 1 et 3");
+}
+
+document.querySelector("#result").textContent =
+  "Merci, rappel de votre chiffre" + response;
